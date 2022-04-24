f90ff4e151e918185894c8a418e0bf9330d50f95
grad73 <gradowski.kam@gmail.com>
2022-04-24 12:31:36 +0200

    Added a new device with the ability to connect to the blynk server.
    Now two devices can get a connection to the blynk server.
    Yellow - New, test device for continuous use with aquarium.
    Green - Previous device, device created to develop the design and test new features.

--- Blynk__free_michelle_try1/src/main.cpp
+++ Blynk__free_michelle_try1/src/main.cpp
@@ -8,9 +8,17 @@
  *************************************************************/
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
+
+/* Green - Dev
 #define BLYNK_TEMPLATE_ID           "TMPLS6OU4IQQ"
 #define BLYNK_DEVICE_NAME           "GRAD Quickstart Device"
 #define BLYNK_AUTH_TOKEN            "6-UIFG5EkFyAbtLATzif3RagR20M8ey2"
+*/
+
+/* Yellow - Testing */
+#define BLYNK_TEMPLATE_ID            "TMPLS6OU4IQQ"
+#define BLYNK_DEVICE_NAME            "Yellow Testing"
+#define BLYNK_AUTH_TOKEN             "awghAqKwiw3YCCXhjIzAhxHZpiBxFb66"
 
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
